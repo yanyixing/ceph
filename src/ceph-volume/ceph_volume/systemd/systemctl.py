f021830be632788113493114de437dfe29781106
Alfredo Deza <adeza@redhat.com>
2017-07-26 11:34:39 -0400

    ceph-volume: systemd: add helpers for the ceph-volume systemd unit
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- src/ceph-volume/ceph_volume/systemd/systemctl.py
+++ src/ceph-volume/ceph_volume/systemd/systemctl.py
@@ -36,8 +36,13 @@ def disable_osd(id_):
     return disable(osd_unit % id_)
 
 
+def enable_volume(id_, fsid, device_type='lvm'):
+    return enable(volume_unit % (id_, fsid, device_type))
+
+
 #
 # templates
 #
 
 osd_unit = "ceph-osd@%s"
+volume_unit = "ceph-volume@%s-%s-%s"
